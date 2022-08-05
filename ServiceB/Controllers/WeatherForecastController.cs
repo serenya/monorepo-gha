82c80ddd9a2a1f052f288879aad548d7d10c9d5c
Sergii Rykov <serenya@live.com>
2022-08-05 07:47:25 +0200

    Fix service b

--- ServiceB/Controllers/WeatherForecastController.cs
+++ ServiceB/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ public class WeatherForecastController : ControllerBase
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    }; bla
+    };
 
     private readonly ILogger<WeatherForecastController> _logger;
 
